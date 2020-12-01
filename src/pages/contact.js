8d2f3c65bf344d06151918fbe38a7014f12e8708
HyunM <wisek0226@gmail.com>
2020-12-01 15:12:22 -0800

    Google fonts completed

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,12 +7,18 @@ const contact = () => {
       <section className="contact-page">
         <article className="contact-form">
           <h3>get in touch</h3>
-          <form>
+          <form action="https://formspree.io/f/mgeplden" method="POST">
             <div className="form-group">
-              <input type="text" placeholder="name" className="form-control" />
+              <input
+                type="text"
+                name="name"
+                placeholder="name"
+                className="form-control"
+              />
               <input
                 type="email"
                 placeholder="email"
+                name="email"
                 className="form-control"
               />
               <textarea
